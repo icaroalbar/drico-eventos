a4a39e4f66839c562289e37d4b220cd6700b123a
Icaro Albar <icaro.albar@live.com>
2024-02-13 13:28:40 -0300

    :iphone: feat: responsive header

--- src/partials/Header.tsx
+++ src/partials/Header.tsx
@@ -1,13 +1,14 @@
 import { Button } from "@/components/ui/button";
-import { Instagram } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <section className="bg-header  bg-cover">
+    <section className="bg-header bg-cover">
       <div className="flex h-[90vh] flex-grow items-center justify-start pl-16 backdrop-brightness-50">
         <div className="flex flex-col gap-5">
-          <div className=" text-start">
+          <div className=" text-center md:text-start">
             <h2 className="scroll-m-20 pb-2 text-3xl font-semibold uppercase tracking-tight first:mt-0">
               criando experiências
             </h2>
@@ -36,9 +37,12 @@ export default function Header() {
           size={"icon"}
           className=" absolute bottom-10 right-10 h-14 w-14 rounded-full border bg-primary  text-white hover:bg-[#61AE6D90]"
         >
-          <a href="#">
-            <Instagram size={28} />
-          </a>
+          <Link
+            target="_blank"
+            href={"https://www.instagram.com/dricoeventos/"}
+          >
+            <FaWhatsapp size={30} />
+          </Link>
         </Button>
       </div>
     </section>
