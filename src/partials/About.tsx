bf0be57f93a997c689426b86f73ea47e85febaad
Icaro Albar <icaro.albar@live.com>
2024-02-13 13:32:11 -0300

    :iphone: feat: responsive about

--- src/partials/About.tsx
+++ src/partials/About.tsx
@@ -30,15 +30,15 @@ const data = [
 export default function About() {
   return (
     <section className="flex flex-col items-center justify-around gap-y-10 border-t py-20">
-      <h1 className="max-w-3xl scroll-m-20 text-center text-3xl font-extrabold tracking-tight lg:text-4xl ">
+      <h1 className="lg: max-w-3xl scroll-m-20 px-10 text-center text-2xl font-extrabold tracking-tight lg:text-4xl ">
         Engaje clientes com ações impactantes, transforme-os em
         <span className="text-primary"> fãs do seu negócio!</span>
       </h1>
-      <div className="flex justify-around gap-5">
+      <div className="grid grid-cols-4 justify-around gap-5">
         {data.map((items, index) => (
           <Card
             key={index}
-            className="flex min-w-72  max-w-72 flex-col items-center justify-center rounded-xl"
+            className="col-span-4 flex min-w-72 max-w-72  flex-col items-center justify-center rounded-xl sm:col-span-2 xl:col-span-1"
           >
             <CardHeader>
               <CardTitle>
