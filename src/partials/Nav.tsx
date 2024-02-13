8dcb5f737b89694f7ba53ccf6de33389b9ba8422
Icaro Albar <icaro.albar@live.com>
2024-02-13 13:26:25 -0300

    :iphone: feat: responsive navbar

--- src/partials/Nav.tsx
+++ src/partials/Nav.tsx
@@ -1,8 +1,9 @@
-import { AlignJustify, Hand, Instagram, ScanFace } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 import { Button } from "../components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { FaWhatsapp } from "react-icons/fa";
 import {
   Sheet,
   SheetClose,
@@ -11,6 +12,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Icon from "@/components/ui/icon";
 
 export default function Nav() {
   return (
@@ -37,10 +39,10 @@ export default function Nav() {
           <Button
             asChild
             variant={"ghost"}
-            className="rounded-xl hover:bg-white/20 hover:text-primary"
+            className="hidden rounded-xl hover:bg-white/20 hover:text-primary md:flex"
           >
             <Link href={"#"} className="flex items-center gap-x-2">
-              <ScanFace /> Ative sua marca
+              <Icon name="ScanFace" /> Ative sua marca
             </Link>
           </Button>
           <div className="space-x-3">
@@ -48,11 +50,14 @@ export default function Nav() {
               asChild
               size={"icon"}
               variant={"ghost"}
-              className="rounded-xl hover:bg-white/20 hover:text-primary"
+              className="hidden rounded-xl hover:bg-white/20 hover:text-primary md:inline-flex"
             >
-              <a href="#">
-                <Instagram />
-              </a>
+              <Link
+                target="_blank"
+                href={"https://www.instagram.com/dricoeventos/"}
+              >
+                <Icon name="Instagram" />
+              </Link>
             </Button>
 
             <Sheet>
@@ -62,19 +67,20 @@ export default function Nav() {
                   variant={"ghost"}
                   className="rounded-xl text-primary hover:bg-white/20 hover:text-primary"
                 >
-                  <AlignJustify />
+                  <Icon name="AlignJustify" size={20} />
                 </Button>
               </SheetTrigger>
               <SheetContent>
-                <SheetHeader>
-                  <SheetTitle>Menu</SheetTitle>
+                <SheetHeader className="mb-2 px-2">
+                  <SheetTitle className="flex justify-start">Menu</SheetTitle>
                 </SheetHeader>
+                <Separator />
                 <Link
                   href={"/partners"}
                   className="flex items-center gap-x-2 p-3 hover:bg-primary"
                 >
                   <SheetClose className="flex w-full items-center gap-x-2 text-start">
-                    <Hand /> Seja um parceiro
+                    <Icon name="HeartHandshake" size={20} /> Seja um parceiro
                   </SheetClose>
                 </Link>
                 <Link
@@ -82,7 +88,25 @@ export default function Nav() {
                   className="flex items-center gap-x-2 p-3 hover:bg-primary"
                 >
                   <SheetClose className="flex w-full items-center gap-x-2 text-start">
-                    <ScanFace /> Ative sua marca
+                    <Icon name="ScanFace" size={20} /> Ative sua marca
+                  </SheetClose>
+                </Link>
+                <Link
+                  target="_blank"
+                  href={"https://www.instagram.com/dricoeventos/"}
+                  className="flex items-center gap-x-2 p-3 hover:bg-primary"
+                >
+                  <SheetClose className="flex w-full items-center gap-x-2 text-start">
+                    <Icon name="Instagram" size={20} /> Rede social
+                  </SheetClose>
+                </Link>
+                <Link
+                  target="_blank"
+                  href={"https://www.instagram.com/dricoeventos/"}
+                  className="flex items-center gap-x-2 p-3 hover:bg-primary"
+                >
+                  <SheetClose className="flex w-full items-center gap-x-2 text-start">
+                    <FaWhatsapp size={20} /> Fale conosco
                   </SheetClose>
                 </Link>
               </SheetContent>
