15207ff2d05b410b46528c39653c8cd85553f237
Icaro Albar <icaro.albar@live.com>
2024-05-11 18:46:14 -0300

    feat: new card action

--- src/partials/Actions.tsx
+++ src/partials/Actions.tsx
@@ -11,15 +11,21 @@ import Image from "next/image";
 const data = [
   {
     image: "/action_claro.jpg",
-    title: "Marketing Promocional",
+    title: "marketing promocional",
     description:
       "Blitz, abordagens, panfletagem direcionada, distribuição e apresentação de serviços/produtos, degustação, captação de leads e muito mais. Nosso vasto portifólio permite criar soluções personalizadas, que atraem e engajam os clientes certos.",
   },
   {
     image: "/action_vivo.jpg",
-    title: "Eventos",
+    title: "eventos",
     description:
       "Cenografia, coffee break, promotores, coordenadores e supervisores de evento, entre outros serviços. Conte com nosso time de profissionais para criar e executar eventos corporativos memoráveis, que vão potencializar a exposição da sua marca, além de gerar networking e negócios.",
+  },
+  {
+    image: "/action_endomarketing.jpg",
+    title: "endomarketing",
+    description:
+      "Estratégia muito utilizada nos dias atuais, o endomarketing vem para melhorar a relação entre a empresa e o colaborador. Podem ser oferecidos incentivos, como premiações ou brindes, para valorizar, incentivar e motivar os times de uma empresa.",
   },
 ];
 
@@ -37,13 +43,13 @@ export default function Actions() {
           Ações estratégicas e direcionadas!
         </h1>
       </div>
-      <div className="container grid grid-cols-2 gap-5 py-10">
+      <div className="container flex flex-col items-center gap-5 space-y-5 py-10">
         {data.map((item, index) => (
           <Card
             key={index}
-            className="col-span-2 flex flex-col items-center justify-center rounded-xl border-2 border-primary bg-background/60 md:col-span-1"
+            className="grid max-w-6xl grid-cols-3 items-center justify-center rounded-xl border-2 border-primary bg-background/60 py-5"
           >
-            <CardHeader>
+            <CardHeader className="col-span-3 items-center lg:col-span-1">
               <Image
                 src={item.image}
                 alt="Imagem de evento"
@@ -52,8 +58,8 @@ export default function Actions() {
                 className="rounded-xl border-2 border-white pt-1"
               />
             </CardHeader>
-            <CardContent className="space-y-8 text-center text-4xl">
-              <CardTitle>{item.title}</CardTitle>
+            <CardContent className="col-span-3 space-y-8 text-center text-4xl lg:col-span-2 lg:text-start">
+              <CardTitle className="capitalize">{item.title}</CardTitle>
               <CardDescription className="text-justify text-white">
                 {item.description}
               </CardDescription>
